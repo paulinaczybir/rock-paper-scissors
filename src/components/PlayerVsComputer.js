c256658938813a33befa0207c38ab62d3e9b41b4
Paulina Czybir <paulina.czybir@gmail.com>
2020-07-07 13:58:56 +0100

    Working player and computer choice.

--- src/components/PlayerVsComputer.js
+++ src/components/PlayerVsComputer.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 
 class PlayerVsComputer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerScore: 0,
+      computerScore: 0,
+      playerChoice: null,
+      computerChoice: null
+    }
+  }
+
+
+  getPlayerChoice = (choice) => {
+    this.setState({
+      playerChoice: choice,
+      computerChoice: Math.floor(Math.random() * 3)
+    })
+  }
+
   render() {
     return (
       <div>
-        <div className="score"> 0 : 0 </div>
+        <div className="match-container">
+          <div className="label">Player</div>
+          <div className="score"> {this.state.playerScore} : {this.state.computerScore} </div>
+          <div className="label">Computer</div>
+        </div>
         <div className="match-message">Rock beats scissors. You win!</div>
         <div className="choices">
-          <img src="/rock.png" alt="rock" className="choice" />
-          <img src="/paper.png" alt="paper" className="choice" />
-          <img src="/scissors.png" alt="scissors" className="choice" />
+          <img onClick={() => this.getPlayerChoice(0)} src="/rock.png" alt="rock" className="choice" />
+          <img onClick={() => this.getPlayerChoice(1)} src="/paper.png" alt="paper" className="choice" />
+          <img onClick={() => this.getPlayerChoice(2)} src="/scissors.png" alt="scissors" className="choice" />
         </div>
       </div>
     );
